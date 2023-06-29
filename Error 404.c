c761b8f8514a89c31d8d4068748fa275b115184e
Shafin956 <45786929+Shafin956@users.noreply.github.com>
2023-06-30 05:23:01 +0600

    Add files via upload

---
+++ Error 404.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    printf("Enter your pin to unlock your pc :");
+    scanf("%d",&a);
+    if(a==20){
+
+        printf("Welcome");
+
+    }
+    else{
+        printf("Error 404");
+
+    }
+
+    return 5;
+
+
+
+}
