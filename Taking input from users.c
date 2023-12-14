265f276ba1d75460f7fd10c4a3098234fcfb1047
Shafin956 <45786929+Shafin956@users.noreply.github.com>
2023-12-14 20:08:23 +0600

    Add files via upload

---
+++ Taking input from users.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int  main ()
+{
+    int a,b;
+    a=20;
+    printf("Hello user !! please type the number you want to add with %d\n",a);
+    scanf("%d",&b);
+    printf("%d + %d = %d",a,b,(a+b));
+    return 5;
+
+
+
+}
